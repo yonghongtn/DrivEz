74820b78c2c0e70a61448890962c20e7a078476b
Ng Yin Kit <yinkit.ng.2021@scis.smu.edu.sg>
2022-11-09 23:54:16 +0800

    db for instrucfor info, db config

---
+++ db_config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-app.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDcAD4nPjxFv3P1hUrq-Sv5JT50r84UqI8",
+    authDomain: "drivez-4f327.firebaseapp.com",
+    databaseURL: "https://drivez-4f327-default-rtdb.firebaseio.com",
+    projectId: "drivez-4f327",
+    storageBucket: "drivez-4f327.appspot.com",
+    messagingSenderId: "198435132181",
+    appId: "1:198435132181:web:0739c1ce18ab34d7154f1c"
+};
+
+initializeApp(firebaseConfig);
