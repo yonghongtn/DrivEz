4c4fb003240e7f8df875040b01b8e0c3ffc84f70
samuel <samuel1.2021@scis.smu.edu.sg>
2022-11-13 18:57:26 +0800

    Update search_instructor.js

--- js/search_instructor.js
+++ js/search_instructor.js
@@ -131,7 +131,10 @@ const app = Vue.createApp({
             var dbRef= sRef(realdb)
             await gett(child(dbRef, "images/"+username+"/licence")).then((snapshot)=>{
                 if(snapshot.exists()){
-                    return snapshot.val().imageurl;
+                    returnedurl=snapshot.val().imageurl;
+                    console.log(returnedurl)
+                    console.log(typeof returnedurl)
+                    return returnedurl
                 }})
            },
 
