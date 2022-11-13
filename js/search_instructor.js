ae82a830699b9e553f8b854c3760d5978ab9f413
samuel <samuel1.2021@scis.smu.edu.sg>
2022-11-13 18:46:56 +0800

    change to async

--- js/search_instructor.js
+++ js/search_instructor.js
@@ -1,6 +1,6 @@
 import {ref, set, get, update} from "https://www.gstatic.com/firebasejs/9.13.0/firebase-database.js"
 import {db} from './db_config.js'
-import{getDatabase, ref, set ,child, get, update, remove} from "https://www.gstatic.com/firebasejs/9.13.0/firebase-database.js";
+import{getDatabase, ref as sRef, set ,child, get, update, remove} from "https://www.gstatic.com/firebasejs/9.13.0/firebase-database.js";
 const realdb= getDatabase();
 
 const app = Vue.createApp({
@@ -126,14 +126,14 @@ const app = Vue.createApp({
 
         
 
-        GetURLfromRealtimeDb(username){
+        async GetURLfromRealtimeDb(username){
    
-            var dbRef= ref(realdb)
-            get(child(dbRef, "images/"+username+"/licence")).then((snapshot)=>{
+            var dbRef= sRef(realdb)
+            await get(child(dbRef, "images/"+username+"/licence")).then((snapshot)=>{
                 if(snapshot.exists()){
                     return snapshot.val().imageurl;
-    
-           }
+                }})
+           },
 
     },
 
